e65e43abcdf8cec22ea5e0bca9d5793666175d12
Michele Fumagalli <michele.fumagalli@unimib.it>
2022-11-28 14:03:56 +0100

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+with open ("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
+setup(
+    name = "nmfpm",
+    version = "1.0",
+    author = "Alessia Longobardi",
+    author_email = "alessia.longobardi@gmail.com",
+    description = "NMFPM profile maker",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url="https://github.com/alongobardi/NMFPM",
+    package_dir = {"": "."},
+    packages={'nmfpm', 'nmfpm.docs'},
+    package_data={"": ['docs/.json','docs/.txt']},
+    include_package_data=True,
+    python_requires=">=3.4",
+    install_requires=['numpy','pandas','astropy']
+
+)
